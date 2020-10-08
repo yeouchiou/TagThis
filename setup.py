3b6f443774c89aeef57766720f0b813a3abb0a9b
Yeou Chiou <yeouchiou@gmail.com>
2020-10-08 23:16:28 +0000

    updated imports

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
-from distutils.core import setup
+from setuptools import setup 
 
 setup(name = "TagThis",
         author = "Yeou Chiou",
         author_email = "yeouchiou@gmail.com",
         url = "https://github.com/yeouchiou/TagThis",
-        packages = ['TagThis']
+        packages = ['TagThis'],
+        include_package_data = True
+
 )
