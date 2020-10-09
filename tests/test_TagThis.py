985569fcfc5204ee0e73f3765c885f5d5e91ece5
Yeou Chiou <yeouchiou@gmail.com>
2020-10-09 00:11:31 +0000

    update simpler test

--- tests/test_TagThis.py
+++ tests/test_TagThis.py
@@ -1,9 +1,12 @@
 import numpy as np
 import numpy.testing as npt
-
 import TagThis
 
 def test_TagThis_smoke():
     #Smoke test
-    doc_list, words, corpus = TagThis.preprocess.createCorpus()
-    obj = TagThis.TopicModel(10, doc_list, words, corpus)
+    from gensim.test.utils import common_texts
+    from gensim.corpora.dictionary import Dictionary
+    common_dictionary = Dictionary(common_texts)
+    common_corpus = [common_dictionary.doc2bow(text) for text in common.texts]
+
+    obj = TagThis.TopicModel(1, common_texts, common_dictionary, common_corpus)
