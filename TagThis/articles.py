2287f236673de0f920ea3770fd078fca2c868f82
Yeou Chiou <yeouchiou@gmail.com>
2020-10-13 05:37:33 +0000

    add createNLPPipeline function

--- TagThis/articles.py
+++ TagThis/articles.py
@@ -1,11 +1,20 @@
 from collections import defaultdict
 import pandas as pd
+import re
+import pickle
+import os
+from tqdm import tqdm
+from gensim import corpora
+from gensim.utils import simple_preprocess
+import spacy
+from spacy.lang.en.stop_words import STOP_WORDS
 
 
 class Articles():
     def __init__(self, filepath):
         self.filepath = filepath
         self.df = self._preprocessToDF()
+        self.doc_list, self.words, self.corpus = self._createCorpus()
 
     def _preprocessToDF(self, urls=True):
         texts = []
@@ -41,3 +50,32 @@ class Articles():
         # Need to alter this if using a different dataset
         df.drop([3978, 7096, 7108, 8869], inplace=True)
         return df
+
+    def createNLPPipeline(self):
+        def lemmatizer(doc):
+            # This takesself, doc of tokens from the NER and lemmatizes them.
+            # Pronouns (like "I" and "you" get lemmatized to '-PRON-', so rem ove those.
+            doc = [token.lemma_ for token in doc if token.lemma_ != '-PRON-']
+            doc = u' '.join(doc)
+            return nlp.make_doc(doc)
+
+        def remove_stopwords(doc):
+            # This will removeself, ords and punctuation.
+            # Use token.text to return strings, which we'll need for Gensim.
+            doc = [token.text for token in doc if not token.is_stop and not token.is_punct]
+            return doc
+
+        nlp = spacy.load('en_core_web_sm', disable=['tagger', 'parser'])
+
+        stop_list = ["mrs", "ms", "say", "'s", "mr", '\ufeff1']
+        nlp.Defaults.stop_words.update(stop_list)
+
+        for word in STOP_WORDS:
+            lexeme = nlp.vocab[word]
+            lexeme.is_stop = True
+
+        # The add_pipe function appends our functions to the default pipeline.
+        nlp.add_pipe(lemmatizer, name='lemmatizer', after='ner')
+        nlp.add_pipe(remove_stopwords, name="stopwords", last=True)
+        return nlp
+
