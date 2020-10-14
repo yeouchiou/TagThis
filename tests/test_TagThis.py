4fe57bc54d82a461f370dd82bb8b3dfe3f2ce019
Yeou Chiou <yeouchiou@gmail.com>
2020-10-14 09:33:01 +0000

    fixed droprows

--- tests/test_TagThis.py
+++ tests/test_TagThis.py
@@ -4,9 +4,9 @@ import TagThis
 
 def test_Articles_smoke():
     # Smoke test
-    obj = TagThis.Articles('data/test.txt')
+    obj = TagThis.Articles('data/test.txt', droprows=[])
 
 def test_TagThis_smoke():
     # Smoke test
-    news = TagThis.Articles('data/test.txt')
+    news = TagThis.Articles('data/test.txt', droprows=[])
     obj = TagThis.TopicModel(1, news)
