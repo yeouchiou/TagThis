d99555cd5567a019be625b1259edd71966264287
Yeou Chiou <yeouchiou@gmail.com>
2020-10-14 09:10:47 +0000

    bugfix

--- tests/test_TagThis.py
+++ tests/test_TagThis.py
@@ -4,9 +4,9 @@ import TagThis
 
 def test_Articles_smoke():
     # Smoke test
-    obj = Articles('data/test.txt')
+    obj = TagThis.Articles('data/test.txt')
 
 def test_TagThis_smoke():
     # Smoke test
-    news = Articles('data/test.txt')
+    news = TagThis.Articles('data/test.txt')
     obj = TagThis.TopicModel(1, news)
